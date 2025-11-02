a9f6bafbbfcf254ad550f3f1adcc777662c8afca
Gaël <33gael.dev@gmail.com>
2025-11-02 12:45:35 +0100

    srcs ft_printf h v1

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 15:36:00 by gaeducas          #+#    #+#             */
-/*   Updated: 2025/11/02 12:38:21 by gaeducas         ###   ########.fr       */
+/*   Updated: 2025/11/02 12:45:16 by gaeducas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <unistd.h>
+# include "../ft_printf/srcs/srcs.h"
 
 int	ft_printf(const char *, ...);
 
--- srcs/srcs.h
+++ srcs/srcs.h
@@ -6,12 +6,16 @@
 /*   By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 12:37:24 by gaeducas          #+#    #+#             */
-/*   Updated: 2025/11/02 12:41:45 by gaeducas         ###   ########.fr       */
+/*   Updated: 2025/11/02 12:44:58 by gaeducas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef SRCS_H
+#ifndef SRCS_H
 # define SRCS_H
+
+# include "../ft_printf.h"
+# include <stdarg.h>
+# include <unistd.h>
 
 int		putchar(int c);
 void	ft_putstr(char *s);
